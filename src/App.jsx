181c73a1b789de5a6dc07cd95a365e4c7338f177
DESKTOP-OSS6MQ7\suits <suitsflame@gmail.com>
2025-07-27 01:07:32 +0300

    implement state for new todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 
 function App() {
+  const [newTodo, setNewTodo] = useState(
+    'New todo will appear here'
+  );
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
