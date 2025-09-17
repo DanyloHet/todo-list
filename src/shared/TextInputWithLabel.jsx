a9d0a7d52dfb3b6c99d9c10a9100c28001a230fd
DanyloHet <danylohet@gmail.com>
2025-09-17 05:11:37 +0300

    adding style for text input label component

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,15 +1,20 @@
+import styled, { css } from 'styled-components';
+const StyledInput = styled.input`
+  border-radius: 6px;
+  color: black;
+`;
 function TextInputWithLabel({ elementId, labelText, onChange, ref, value }) {
   return (
     <>
       <label htmlFor={elementId}>{labelText}</label>
-      <input
+      <StyledInput
         type="text"
         name="title"
         id={elementId}
         ref={ref}
         value={value}
         onChange={onChange}
-      ></input>
+      ></StyledInput>
     </>
   );
 }
